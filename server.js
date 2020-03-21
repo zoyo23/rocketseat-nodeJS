5cd62fb8619b9726e4653a0590bc8f077974584f
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-20 21:01:26 -0300

    [AULA 08] Conectando Database

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNerParser: true });
+
+//Primeira rota
 app.get("/", (req, res) => {
     res.send("Hello rocketseat");
 });
