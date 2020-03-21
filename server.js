5d6b5ff7b5caf83442009354f2ade50e74718508
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-21 10:35:58 -0300

    [AULA 10] Reestruturação de arquivos

--- server.js
+++ server.js
@@ -6,20 +6,13 @@ const requireDir = require('require-dir');
 const app = express();
 
 //Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNerParser: true });
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNerParser: true }
+);
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
+// Rotas
+app.use('/api', require('./src/routes'));
 
-
-//Primeira rota
-app.get("/", (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native app with React',
-        url: 'http://github.com/facebook/react-native'
-    });
-
-    return res.send("Hello rocketseat");
-});
 app.listen(3001);
