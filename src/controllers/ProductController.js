70cbfb7a33c8fbc66005c13a6b83dc78c374a27b
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-03-24 08:47:04 -0300

    [AULA 13] CRUD

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,15 +4,41 @@ const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res) {
-        const products = await Product.find();
+        const products = await Product
+            .find();
 
         return res.json(products);
     },
 
-    async store(req, res) {
-        //Criação
-        const product = await Product.create(req.body);
+    async show(req, res) {
+        const product = await Product
+            .findById(req.params.id);
 
         return res.json(product);
+    },
+
+    async store(req, res) {
+        //Criação
+        const product = await Product
+            .create(req.body);
+
+        return res.json(product);
+    },
+
+    async udpate(req, res) {
+        const product = await Product
+            .findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                { new: true }
+            );
+
+        return res.json(product);
+    },
+
+    async destroy(req, res) {
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
 routes.get("/products", ProductController.index);
+routes.get("/products/:id", ProductController.show);
 routes.post("/products", ProductController.store)
+routes.put("/products/:id", ProductController.udpate)
+routes.delete("/products/:id", ProductController.destroy)
 
 module.exports = routes;
